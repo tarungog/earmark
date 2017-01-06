74823948ebf5e27f361236b7adf0a35e26967cf2
tgog <tgog@umich.edu>
2017-01-05 20:54:27 -0500

    Should work???

--- content.js
+++ content.js
@@ -1,4 +1,14 @@
 // content.js
-var parent = document.getElementById("js_1");
-var child = document.getElementById("_497p _11es");
-parent.removeChild(child);
+
+function remove_games() 
+{
+    var parent = document.getElementById("js_1");
+    var child = document.getElementsByClassName("_497p _11es");
+    var childLength = child.length;
+    while(child.length > 0){
+        parent.removeChild(child[0]);
+    }
+}
+
+document.getElementById("js_1").onchange  = remove_games;
+window.onload = removeChild;
